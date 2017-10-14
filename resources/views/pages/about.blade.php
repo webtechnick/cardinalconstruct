b2d11e2e266da70ee883fd18a6fe5415766a1380
Nick Baker <nick@webtechnick.com>
2017-10-14 10:21:47 -0600

    Adding DYC windows

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts/app')
+
+@section('content')
+    <h1>About Us</h1>
+	<hr>
+@stop
---
+++ resources/views/pages/dycwindows.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts/app')
+
+@section('content')
+    <h1>DYC Windows</h1>
+	<hr>
+@stop
