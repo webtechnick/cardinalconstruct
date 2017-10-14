7f28c8972cf99ade6a6829aa7b5f77666f8b5a26
Nick Baker <nick@webtechnick.com>
2017-10-14 11:05:59 -0600

    Add fav icon

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'cardinalinc505@aol.com', 'name' => 'Cardinal Construction'],
 
     /*
     |--------------------------------------------------------------------------
