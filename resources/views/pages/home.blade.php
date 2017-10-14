2f292ba1f183888b2cfad1d6d2890c629b344d3c
Nick Baker <nick@webtechnick.com>
2017-10-14 10:58:47 -0600

    Nicer homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,8 +1,35 @@
 @extends('layouts/app')
 
 @section('content')
-	<div class="jumbotron">
-		<h1>Cardinal Construction</h1>
-		<p>(505) 268-2738 | <a href="mailto:Cardinalinc505@aol.com">Cardinalinc505@aol.com</a></p>
-	</div>
+
+    <img src="/images/logo_fb.jpg" class="img-rounded img-responsive">
+
+    <div class="row">
+
+        <div class="col-md-4 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading text-right">
+                    Contact Us
+                </div>
+                <div class="panel-body text-right">
+                    <p>Phone: (505) 268-2738</p>
+                    <p>Fax: (505) 268-2777</p>
+
+                    <p>Email: <a href="mailto:Cardinalinc505@aol.com">Cardinalinc505@aol.com</a></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Connect With Us
+                </div>
+                <div class="panel-body text-left">
+                    <p>Facebook: <a href="https://www.facebook.com/cardinalconstructioninc/">Cardinal Construction Inc</a></p>
+                    <p>Yelp: <a href="https://www.yelp.com/biz/cardinal-construction-albuquerque">Cardinal Construction Inc</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
